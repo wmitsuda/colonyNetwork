c9f599e835b4d2b52bc9289d177657e89292a479
elenadimitrova <elenavdimitrova@gmail.com>
2018-10-30 18:23:47 +0200

    Origin reputation validate checks

--- test/colony-network-mining.js
+++ test/colony-network-mining.js
@@ -1805,7 +1805,7 @@ contract("ColonyNetworkMining", accounts => {
       await repCycle.confirmNewHash(1);
     });
 
-    it("if a colony wide total calculation as a result of a child reputation update is wrong, it should be handled correctly", async () => {
+    it("if a colony wide total calculation (for a parent skill) is wrong, it should be handled correctly", async () => {
       await giveUserCLNYTokensAndStake(colonyNetwork, MAIN_ACCOUNT, DEFAULT_STAKE);
       await giveUserCLNYTokensAndStake(colonyNetwork, OTHER_ACCOUNT, DEFAULT_STAKE);
 
@@ -1861,8 +1861,8 @@ contract("ColonyNetworkMining", accounts => {
 
       badClient = new MaliciousReputationMinerExtraRep(
         { loader: contractLoader, minerAddress: OTHER_ACCOUNT, realProviderPort: REAL_PROVIDER_PORT, useJsTree },
-        32,
-        "0xf"
+        28,
+        "0xffff"
       );
       // Moving the state to the bad client
       await badClient.initialise(colonyNetwork.address);
@@ -1934,7 +1934,7 @@ contract("ColonyNetworkMining", accounts => {
 
       badClient = new MaliciousReputationMinerExtraRep(
         { loader: contractLoader, minerAddress: OTHER_ACCOUNT, realProviderPort: REAL_PROVIDER_PORT, useJsTree },
-        27,
+        32,
         "0xfffffffff"
       );
       // Moving the state to the bad client
@@ -2007,7 +2007,7 @@ contract("ColonyNetworkMining", accounts => {
 
       badClient = new MaliciousReputationMinerExtraRep(
         { loader: contractLoader, minerAddress: OTHER_ACCOUNT, realProviderPort: REAL_PROVIDER_PORT, useJsTree },
-        31,
+        30,
         "900000000000"
       );
       // Moving the state to the bad client
@@ -2156,7 +2156,7 @@ contract("ColonyNetworkMining", accounts => {
       await repCycle.confirmNewHash(1);
     });
 
-    it("if main skill (in a negative update) reputation calculation is wrong, it should be handled correctly", async () => {
+    it("if child skill reputation calculation is wrong, it should be handled correctly", async () => {
       await giveUserCLNYTokensAndStake(colonyNetwork, MAIN_ACCOUNT, DEFAULT_STAKE);
       await giveUserCLNYTokensAndStake(colonyNetwork, OTHER_ACCOUNT, DEFAULT_STAKE);
 
@@ -2229,7 +2229,7 @@ contract("ColonyNetworkMining", accounts => {
       await repCycle.confirmNewHash(1);
     });
 
-    it("if a colony-wide main skill reputation amount calculation underflows and is wrong, it should be handled correctly", async () => {
+    it("if a colony-wide child skill reputation amount calculation underflows and is wrong, it should be handled correctly", async () => {
       await giveUserCLNYTokensAndStake(colonyNetwork, MAIN_ACCOUNT, DEFAULT_STAKE);
       await giveUserCLNYTokensAndStake(colonyNetwork, OTHER_ACCOUNT, DEFAULT_STAKE);
 
